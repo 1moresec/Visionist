831cafd28cf8efe1a0c5526546dc1bc77f910b37
CuiXiaohan <cuixiaohan@me.com>
2017-12-24 00:01:30 +0800

    Roll back test

--- Visionist/ViewController.swift
+++ Visionist/ViewController.swift
@@ -11,13 +11,17 @@ import UIKit
 class ViewController: UIViewController {
     
     //MARK: Properties
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var ArtworkNameLabel: UILabel!
+//    @IBOutlet weak var nameTextField: UITextField!
+//    @IBOutlet weak var ArtworkNameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //MARK: Actions
+//    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+//        ArtworkNameLabel.text = "A Piece of This World"
+//    }
 }
 
